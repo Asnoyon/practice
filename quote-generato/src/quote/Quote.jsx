64a98e43e6de78222d6a2e815f546d541842c1fa
Asnoyon <blackpain1100@gmail.com>
2023-09-13 23:27:49 +0600

    handleClick function add

--- quote-generato/src/quote/Quote.jsx
+++ quote-generato/src/quote/Quote.jsx
@@ -3,30 +3,38 @@ import React, { useEffect, useState } from 'react'
 const Quote = () => {
     const [quote,setQuote]=useState("");
     const [author,setAuthor]=useState("")
+
+    const fetchData = async()=>{
+        const res = await fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+
+        const data= await res.json();
+
+        let dataQuotes = data.quotes;
+        
+        let randomNum = Math.floor(Math.random()* dataQuotes.length)
+
+        let randomQuotes = dataQuotes[randomNum]
+       
+        
+        setQuote(randomQuotes.quote)
+        setAuthor(randomQuotes.author)
+    }
+    
     useEffect(() => {
-        const fetchData = async()=>{
-            const res = await fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
-
-            const data= await res.json();
-
-            let dataQuotes = data.quotes;
-            
-            let randomNum = Math.floor(Math.random()* dataQuotes.length)
-
-            let randomQuotes = dataQuotes[randomNum]
-           
-            
-            setQuote(randomQuotes.quote)
-            setAuthor(randomQuotes.author)
-        }
-        fetchData()
+        fetchData()  
     }, [])
     
+    const handleClick= ()=>{
+        fetchData()
+    }
 
   return (
     <div>
-        <p>{quote}</p>
+       <div className="quote">
+       <p>{quote}</p>
         <h2>{author}</h2>
+       </div>
+       <button onClick={handleClick}>Next Quote</button>
     </div>
   )
 }
