7cd78b10b20b9745178b939c2952c9e6b2952c4f
Asnoyon <blackpain1100@gmail.com>
2023-10-03 22:25:01 +0600

    state update

--- core/src/component/state.jsx
+++ core/src/component/state.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const State = () => {
+    const [count,setCount]=useState(0);
+    const handleClick = ()=>{
+        setCount(count+1)
+    }
+    const handleMinusClick = ()=>{
+        if(count!==0){
+            setCount(count-1)
+        }
+    }
   return (
-    <div>S</div>
+    <>
+        <div>
+            <button onClick={handleMinusClick}>-</button>
+            <h1>{count}</h1>
+            <button onClick={handleClick}>+</button>
+        </div>
+    </>
   )
 }
 
